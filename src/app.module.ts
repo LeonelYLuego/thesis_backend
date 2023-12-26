a9a081f97064ab40af07efc3eb97724ec0181519
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-12-26 11:20:37 -0600

    Configuration project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.DEV_MODE != 'false',
+      autoLoadEntities: process.env.DEV_MODE != 'false',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
